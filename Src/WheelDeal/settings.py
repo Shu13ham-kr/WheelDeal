49c119b21ccfca88d457de7f1bb678e30b79a757
Shubham Kumar <shubham01210@gmail.com>
2024-10-05 02:29:08 +0530

    Update settings.py

--- Src/WheelDeal/settings.py
+++ Src/WheelDeal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django-insecure-ep$0t=-gcw&%+#*vo$z864i3c0&33()c56rvwmkv1xlw$fshih
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if env('DJANGOAPPMODE') == 'DEBUG' else False
